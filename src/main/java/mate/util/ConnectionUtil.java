e752f7ba93ed68fd697c4c2c9869b07a91a45c5f
mankokolya <mankokolya@gmail.com>
2021-06-09 20:52:59 +0300

    add heroku dependency and remote mysql database

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,10 +6,9 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    public static final String URL = "jdbc:mysql://localhost:3306/"
-            + "manufacturer_db?serverTimezone=UTC";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "0909";
+    public static final String URL = "jdbc:mysql://remotemysql.com:3306/advGh2QwkX?serverTimezone=UTC";
+    public static final String USERNAME = "advGh2QwkX";
+    public static final String PASSWORD = "BFzU3ZpRVE";
     public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
     static {
